1aacd8c45f29e851cff77442dfa95a9aa8f07d7c
JuanPabloBallesteros1905 <jpballesterossosa@gmail.com>
2024-04-04 23:08:58 -0500

    guardar avances

--- lib/peticiones/peticiones.dart
+++ lib/peticiones/peticiones.dart
@@ -1,18 +1,12 @@
 import 'package:dio/dio.dart';
 
-
 class Peticioes {
-
-
   dynamic server;
 
-  Future<void> getInfoServer( String endPoint ) async {
-
-    if (endPoint.isEmpty ) {
-
-      
-    }else{
-      final response = await Dio().get( 'https://api.mcsrvstat.us/2/$endPoint' );
+  Future<void> getInfoServer(String endPoint) async {
+    if (endPoint.isEmpty) {
+    } else {
+      final response = await Dio().get('https://api.mcsrvstat.us/2/$endPoint');
 
       print('https://api.mcsrvstat.us/2/$endPoint');
 
